f4111eb699cc50aeb688b98784e5f9f1b83cbce3
Lauren Birts <59886502+lbirts@users.noreply.github.com>
2020-06-25 11:47:29 -0500

    fin

--- src/PoemList.js
+++ src/PoemList.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { PoemCard } from './PoemCard';
 
-export function PoemList(){
+export function PoemList(props) {
+    
     return (
+        // console.log(props),
         <div className="ui cards" style={{ marginTop: 20 }}>
-        {[ /*  Replace this with an array of poems */ ].map( poem => (
+        {props.poems.map((poem, index) => (
             <PoemCard
+                deletePoem={props.deletePoem}
+                updatePoems={props.updatePoems}
+                index={index}
                 name={poem.name}
                 content={poem.content}
             />
+           
         ))}
         </div>
     )
